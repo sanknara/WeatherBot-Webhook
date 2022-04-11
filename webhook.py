1747627c741e303910f670bc1fa5a3a612ca45b4
Sankara Narayanan <69718285+sanknara@users.noreply.github.com>
2022-04-11 22:09:34 +0530

    Commit weather forecast webhook

---
+++ webhook.py
@@ -0,0 +1,66 @@
+import json
+import os
+import requests
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    print(json.dumps(req, indent=4))
+    
+    res = makeResponse(req)
+    
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeResponse(req):
+    if req.get("queryResult").get("action") != "fetchWeatherForecast":
+        return {}
+    result = req.get("queryResult")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    date = parameters.get("date-time")
+    date = date[:10]
+    if city is None:
+        return None
+    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=3f0387334624d977b626208dde397bcd')
+    json_object = r.json()
+    weather=json_object['list']
+    condition= "?"
+    for i in range(0,30):
+        if date in weather[i]['dt_txt']:
+            condition= weather[i]['weather'][0]['description']
+            break
+    speech = "The forecast for"+city+ "for "+date+" is "+condition
+   return { "fulfillmentText": speech }
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
